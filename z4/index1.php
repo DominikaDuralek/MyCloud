37d037fec9cddd31aaa2e437d4c0aef62d97d4f3
DominikaDuralek <73932058+DominikaDuralek@users.noreply.github.com>
2022-11-22 16:33:57 +0100

    Poprawne działanie aplikacji
    Wszystkie funkcjonalności działają poprawnie

--- z4/index1.php
+++ z4/index1.php
@@ -43,25 +43,28 @@ if (!isset($_SESSION['loggedin']))
 		else{
 			echo "<br>Aktualny katalog: " . $_SESSION['current_dir'] . "<br><br>";
 		}
+		
+		if($_SESSION['current_dir'] == $main_dir){ //jezeli znajdujemy sie w katalogu glownym uzytkownika - mozna tworzyc podkatalogi
 	?>
 		
-		<br><form action="index1.php" method="post">
-				<button type="submit" name="makedir" style="border:none;background-color:#ffffff;">
-                    <i class="fa-solid fa-folder-plus" type="submit" name="makedir" style="font-size:34px;"></i>
-                </button>
-		</form><br>
-				
+			<br><form action="index1.php" method="post">
+					<button type="submit" name="makedir" style="border:none;background-color:#ffffff;">
+						<i class="fa-solid fa-folder-plus" type="submit" name="makedir" style="font-size:34px;"></i>
+					</button>
+			</form><br>
+					
 		<?php
-		    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['makedir']))
-			{
-				make_dir();
-			}
-			function make_dir() { //funkcja do tworzenia nowego katalogu
-				echo "<br>Nazwa nowego katalogu<form action='newdir.php' method='post'>
-						<input type='text' name='dir_name' maxlength='20' size='20'>
-							<input type='submit' value='Send'/>
-						</form><br>";
-			}
+		}
+				if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['makedir']))
+				{
+					make_dir();
+				}
+				function make_dir() { //funkcja do tworzenia nowego katalogu
+					echo "<br>Nazwa nowego katalogu<form action='newdir.php' method='post'>
+							<input type='text' name='dir_name' maxlength='20' size='20'>
+								<input type='submit' value='Send'/>
+							</form><br>";
+				}
 		?>
 		
 		<label for="uploaded_file">
@@ -75,20 +78,24 @@ if (!isset($_SESSION['loggedin']))
 		
 		<br>Lista katalogów użytkownika:
 		<br><br>
-		<form action='levelup.php' method='post'>
-				<button type="submit" name="levelup" style="border:none;background-color:#ffffff;">
-                    <i class="fa-solid fa-circle-arrow-left" type="submit" name="levelup" style="font-size:28px;"></i>
-                </button>
-		</form>
 		
+		<?php if($_SESSION['current_dir'] != $main_dir){ //jezeli nie znajdujemy sie w katalogu glownym uzytkownika - mozna klinkac 'levelup'
+		?>
+			<form action='levelup.php' method='post'>
+					<button type="submit" name="levelup" style="border:none;background-color:#ffffff;">
+						<i class="fa-solid fa-circle-arrow-left" type="submit" name="levelup" style="font-size:28px;"></i>
+					</button>
+			</form>
 		<?php
+		}
+		
 		$current_dir = $_SESSION['current_dir'];
-        $main_dir_content = array_filter(glob($main_dir . "*")); //wszystko, co znajduje sie w katalogu glownym
-		echo "<a href ='movedir.php?current_dir=$main_dir'>" . $main_dir . "<a><br>"; //glowny katalog uzytkownika
-		foreach ($main_dir_content as $content) { //kazdy element katalogu glownego
+        $current_dir_content = array_filter(glob($current_dir . "*")); //wszystko, co znajduje sie w katalogu glownym
+		echo "<a href ='movedir.php?current_dir=$current_dir'>" . $current_dir . "<a><br>"; //glowny katalog uzytkownika
+		foreach ($current_dir_content as $content) { //kazdy element katalogu glownego
 			if(is_dir($content)){ //jesli element jest katalogiem
 				echo "&ensp;<a href ='movedir.php?current_dir=$content/'>" . $content . '/' . "</a>"; //&ensp - duza spacja, link do pobrania pliku
-				echo "&nbsp&nbsp;<a href='removecontent.php?content_to_remove=$content'><i class='fa-regular fa-trash-can' style='font-size:14px;'></i></a><br>"; //ikonka kosza
+				echo "&nbsp&nbsp;<a href='removecontent.php?content_to_remove=$content/'><i class='fa-regular fa-trash-can' style='font-size:14px;'></i></a><br>"; //ikonka kosza
 			}
 			else{ //jesli element nie jest katalogiem (plik)
 				echo "&ensp;<a href='$content' download>" . $content . "</a>"; //link do pobrania pliku
@@ -104,25 +111,6 @@ if (!isset($_SESSION['loggedin']))
 				}
 				echo "&nbsp;<a href='$content'><i class='$media_icon' style='font-size:14px;'></i></a>";
 				echo "&nbsp&nbsp;<a href='removecontent.php?content_to_remove=$content'><i class='fa-regular fa-trash-can' style='font-size:14px;'></i></a><br>"; //ikonka kosza							
-			}
-			
-			if(count(glob($content . '/*')) !== 0){ //jesli podkatalog nie jest pusty
-				$sub_dir_content = array_filter(glob($content . '/*')); //wszystko, co znajduje sie w podkatalogu
-				foreach ($sub_dir_content as $sub_content) {
-					echo "&ensp;&ensp;<a href='$sub_content' download>" . $sub_content . "</a>"; //link do pobrania pliku
-					$file_extension = substr($sub_content, strpos($sub_content, ".") + 1); //pobranie rozszerzenia
-					if($file_extension == "png" || $file_extension == "jpg" || $file_extension == "jpeg" || $file_extension == "gif"){
-						$media_icon = 'fa-regular fa-image'; //ikona - obraz
-					}
-					if($file_extension == "mp4"){
-						$media_icon = 'fa-solid fa-video'; //ikona - wideo
-					}
-					if($file_extension == "mp3"){
-						$media_icon = 'fa-solid fa-volume-high'; //ikona - audio
-					}
-					echo "&nbsp;<a href='$sub_content'><i class='$media_icon' style='font-size:14px;'></i></a>";
-					echo "&nbsp&nbsp;<a href='removecontent.php?content_to_remove=$sub_content'><i class='fa-regular fa-trash-can' style='font-size:14px;'></i></a><br>"; //ikonka kosza	
-				}
 			}
 		}
 		
--- z4/removecontent.php
+++ z4/removecontent.php
@@ -3,7 +3,16 @@
 	session_start();
 	$content_to_remove = $_GET['content_to_remove'];
 	if(is_dir($content_to_remove)){ //jesli element do usuniecia jest katalogiem
-		rmdir($content_to_remove);
+	    $dir_content = array_filter(glob($content_to_remove . "*"));  //elementy wewnatrz katalogu do usuniecia
+		foreach ($dir_content as $content) {
+			unlink($content);	
+			$link = mysqli_connect('mariadb106.server701675.nazwa.pl', 'server701675_domdur1', '6D6zB4WuURKzU@h', 'server701675_domdur1'); // połączenie z BD
+			if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); } // obsługa błędu połączenia z BD
+			$sql = "DELETE FROM files WHERE concat(location, file_name) = '$content'"; //usuniecie pliku z bazy
+			mysqli_query($link, $sql);
+			mysqli_close($link);
+		}
+		rmdir($content_to_remove); //usuniecie katalogu po usunieciu jego plikow
 	}
 	else{ //element do usuniecia jest plikiem
 		unlink($content_to_remove);	
