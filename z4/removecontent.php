51d22c77b51a54c5181f29fbd12e426818dd71e1
DominikaDuralek <73932058+DominikaDuralek@users.noreply.github.com>
2022-11-30 18:38:55 +0100

    Update removecontent.php

--- z4/removecontent.php
+++ z4/removecontent.php
@@ -6,7 +6,7 @@
 	    $dir_content = array_filter(glob($content_to_remove . "*"));  //elementy wewnatrz katalogu do usuniecia
 		foreach ($dir_content as $content) {
 			unlink($content);	
-			$link = mysqli_connect('mariadb106.server701675.nazwa.pl', 'server701675_domdur1', '6D6zB4WuURKzU@h', 'server701675_domdur1'); // połączenie z BD
+			$link = mysqli_connect('', '', '', ''); // połączenie z BD
 			if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); } // obsługa błędu połączenia z BD
 			$sql = "DELETE FROM files WHERE concat(location, file_name) = '$content'"; //usuniecie pliku z bazy
 			mysqli_query($link, $sql);
@@ -16,7 +16,7 @@
 	}
 	else{ //element do usuniecia jest plikiem
 		unlink($content_to_remove);	
-		$link = mysqli_connect('mariadb106.server701675.nazwa.pl', 'server701675_domdur1', '6D6zB4WuURKzU@h', 'server701675_domdur1'); // połączenie z BD
+		$link = mysqli_connect('', '', '', ''); // połączenie z BD
 		if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); } // obsługa błędu połączenia z BD
 		$sql = "DELETE FROM files WHERE concat(location, file_name) = '$content_to_remove'"; //usuniecie pliku z bazy
 		mysqli_query($link, $sql);
